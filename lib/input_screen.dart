cca220c1299a506065b2dfacad92f8d03ea38f0b
Reland Boyle <71753585+relandboyle@users.noreply.github.com>
2024-10-07 06:06:09 -0400

    Main (#1)
    * Error calling OpenAI API
    
    * Working call with local .env file
    
    * Troubleshooting Android gremlins

--- lib/input_screen.dart
+++ lib/input_screen.dart
@@ -33,86 +33,119 @@ class _InputScreenState extends State<InputScreen> {
         ),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              // Display a random greeting
-              const DynamicSalutation(),
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [CognifictColors.lighterBlue, CognifictColors.luminousBlue],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  // Display a random greeting
+                  const DynamicSalutation(),
 
-              // Display the recognized text OR the local response text
-              Card(
-                elevation: 5,
-                clipBehavior: Clip.hardEdge,
-                color: CognifictColors.mediumBlue,
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxHeight: 250, minHeight: 100),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SingleChildScrollView(
-                      child: _localResponseText.isEmpty
-                          ? Text(
-                              speechService.recognizedText.isEmpty ? 'Tap the mic and start speaking...' : speechService.recognizedText,
-                              style: const TextStyle(fontSize: 24.0),
-                            )
-                          : Text(
-                              _localResponseText,
-                              style: const TextStyle(fontSize: 24.0, color: CognifictColors.lighterBlue),
-                            ),
+                  // Display the recognized text OR the local response text
+                  Card(
+                    elevation: 5,
+                    clipBehavior: Clip.hardEdge,
+                    color: CognifictColors.mediumBlue,
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxHeight: 250, minHeight: 100),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: apiProvider.isLoading
+                            ? const Center(
+                                child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(CognifictColors.lighterBlue),
+                              ))
+                            : SingleChildScrollView(
+                                child: _localResponseText.isEmpty
+                                    ? Text(
+                                        speechService.recognizedText.isEmpty ? 'Tap the mic and start speaking...' : speechService.recognizedText,
+                                        style: const TextStyle(fontSize: 24.0),
+                                      )
+                                    : Text(
+                                        _localResponseText,
+                                        style: const TextStyle(fontSize: 24.0, color: CognifictColors.lighterBlue),
+                                      ),
+                              ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 50),
+                  const SizedBox(height: 50),
 
-              // Display the microphone image button
-              Card(
-                elevation: 5,
-                clipBehavior: Clip.hardEdge,
-                child: SizedBox(
-                  width: 350,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: Center(
-                      child: Ink.image(
-                        image: const AssetImage('assets/images/microphone.jpg'),
-                        fit: BoxFit.contain,
-                        width: 350,
-                        height: 200,
-                        child: InkWell(
-                          onTap: speechService.isListening ? speechService.stopListening : () => startListening(),
+                  // Display the microphone image button
+                  Stack(
+                    children: [
+                      Card(
+                        elevation: 5,
+                        clipBehavior: Clip.hardEdge,
+                        child: SizedBox(
+                          width: 350,
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Center(
+                              child: Ink.image(
+                                image: const AssetImage('assets/images/microphone.jpg'),
+                                fit: BoxFit.contain,
+                                width: 350,
+                                height: 200,
+                                child: InkWell(
+                                  onTap: speechService.isListening ? speechService.stopListening : () => startListening(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // a Container that is a small circle with a color that changes based on the listening state
+                      Positioned(
+                        left: 10,
+                        top: 10,
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: speechService.isListening ? Colors.red : CognifictColors.mediumGreen,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 40),
+
+                  // Display the Ask Cognifict button
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: CognifictColors.lighterBlue),
+                    onPressed: () async {
+                      await apiProvider.sendQueryToApi(speechService.recognizedText);
+                      _localResponseText = apiProvider.responseText;
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Ask Cognifict',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: CognifictColors.goldenYellow,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 100),
 
-              // Display the Ask Cognifict button
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: CognifictColors.lighterBlue),
-                onPressed: () async {
-                  await apiProvider.sendQueryToApi(speechService.recognizedText);
-                  _localResponseText = apiProvider.responseText;
-                },
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Ask Cognifict',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: CognifictColors.goldenYellow,
-                    ),
-                  ),
-                ),
+                  // Add extra margin at the bottom
+                  const SizedBox(height: 20),
+                ],
               ),
-
-              // Add extra margin at the bottom
-              const SizedBox(height: 20),
-            ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,9 @@ import 'settings_screen.dart';
 import 'classes/speech_service.dart';
 import 'classes/open_ai_api_provider.dart';
 
-// DotEnv dotenv = DotEnv() is automatically called during import.
-// If you want to load multiple dotenv files or name your dotenv object differently, you can do the following and import the singleton into the relavant files:
-// DotEnv another_dotenv = DotEnv()
 
 Future main() async {
-  // To load the .env file contents into dotenv.
-  // NOTE: fileName defaults to .env and can be omitted in this case.
-  // Ensure that the filename corresponds to the path in step 1 and 2.
   await dotenv.load(fileName: ".env");
-  //...runapp
   runApp(const CognifictApp());
 
 }
