589d003ee0f9ec9076d33a3ac3a39e219954c681
Reland Boyle <71753585+relandboyle@users.noreply.github.com>
2024-10-15 20:25:43 -0400

    Feature/firebase function (#2)
    * Chasing Android / Gradle / Java config issues
    
    * Repairing GitHub workflow
    
    * Limited width of text card; sky blue response text
    
    * New build

--- lib/classes/speech_service.dart
+++ lib/classes/speech_service.dart
@@ -33,7 +33,7 @@ class SpeechService with ChangeNotifier {
     _speech.stop();
     _isListening = false;
     notifyListeners();
-    print(recognizedText);
+    print(_recognizedText);
   }
 
   // Callback function to process the recognized speech
